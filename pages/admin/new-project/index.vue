afe98d5f9c9eaaa7995eaeaeba270f7238492e3a
Evan Ang <alethiologist@gmail.com>
2020-09-13 12:19:39 +0800

    fixed form styling

--- pages/admin/new-project/index.vue
+++ pages/admin/new-project/index.vue
@@ -2,29 +2,37 @@
   <div class="admin-new-project-page">
     <section class="section new-project-form">
       <div class="container">
-        <div class="title">Add New Project</div>
         <div class="columns">
-          <div class="column">
-            <b-field label="Name">
-              <b-input v-model="form.name"></b-input>
+          <div class="column"></div>
+          <div class="column is-two-thirds">
+            <div class="title">Add New Project</div>
+            <div class="field is-horizontal">
+              <div class="field-body">
+                <b-field label="Name">
+                  <b-input v-model="form.name"></b-input>
+                </b-field>
+                <b-field label="Languages Used">
+                  <b-taginput
+                    v-model="tags"
+                    :data="filteredTags"
+                    autocomplete
+                    :allow-new="allowNew"
+                    :open-on-focus="openOnFocus"
+                    icon="tag"
+                    placeholder="Add a tag"
+                    @typing="getFilteredTags"
+                  >
+                  </b-taginput>
+                </b-field>
+              </div>
+            </div>
+
+            <b-field label="Short Summary">
+              <b-input v-model="summary"></b-input>
             </b-field>
-          </div>
-          <div class="column">
-            <b-field label="Languages Used">
-              <b-taginput
-                v-model="tags"
-                :data="filteredTags"
-                autocomplete
-                :allow-new="allowNew"
-                :open-on-focus="openOnFocus"
-                icon="tag"
-                placeholder="Add a tag"
-                @typing="getFilteredTags"
-              >
-              </b-taginput>
+            <b-field label="Description">
+              <b-input v-model="description" type="textarea"></b-input>
             </b-field>
-          </div>
-          <div class="column">
             <b-field label="Thumbnail">
               <b-field
                 class="file is-primary"
@@ -41,21 +49,16 @@
                 </b-upload>
               </b-field>
             </b-field>
+            <div class="field is-grouped is-grouped-centered">
+              <div class="control">
+                <button class="button is-primary">Submit</button>
+              </div>
+              <div class="control">
+                <button class="button is-warning">Cancel</button>
+              </div>
+            </div>
           </div>
-        </div>
-        <b-field label="Short Summary">
-          <b-input v-model="summary"></b-input>
-        </b-field>
-        <b-field label="Description">
-          <b-input v-model="description" type="textarea"></b-input>
-        </b-field>
-        <div class="field is-grouped">
-          <div class="control">
-            <button class="button is-primary">Submit</button>
-          </div>
-          <div class="control">
-            <button class="button is-warning">Cancel</button>
-          </div>
+          <div class="column"></div>
         </div>
       </div>
     </section>
