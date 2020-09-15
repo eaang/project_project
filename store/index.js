19018a10c25439d95ac8cebb721c4a7abd6cd9ec
Evan Ang <alethiologist@gmail.com>
2020-09-15 12:52:32 +0800

    converted store to modular format

--- store/index.js
+++ store/index.js
@@ -1,77 +1,72 @@
-import Vuex from 'vuex'
+export const state = () => ({
+  loadedProjects: [],
+})
 
-const createStore = () => {
-  return new Vuex.Store({
-    state: {
-      loadedProjects: [],
-    },
-    mutations: {
-      setProjects(state, projects) {
-        state.loadedProjects = projects
-      },
-    },
-    actions: {
-      nuxtServerInit(vuexContext, context) {
-        return new Promise((resolve, reject) => {
-          setTimeout(() => {
-            vuexContext.commit('setProjects', [
-              {
-                id: '1',
-                description: 'A project to hold other projects.',
-                github: '',
-                images: [
-                  'projects/gzmpo4abib15yesjaeep',
-                  'projects/sh2dnhcx9nqzypl7ysqw',
-                ],
-                languages: ['HTML', 'CSS', 'Bulma', 'Buefy', 'Vue', 'Nuxt'],
-                link: 'http://testlink.io',
-                name: 'The Project of Projects',
-                progress: 7,
-                summary: 'A project to hold other projects.',
-              },
-              {
-                id: '2',
-                description: 'A project to hold other projects.',
-                github: '',
-                images: [
-                  'projects/gzmpo4abib15yesjaeep',
-                  'projects/sh2dnhcx9nqzypl7ysqw',
-                ],
-                languages: ['HTML', 'CSS', 'Bulma', 'Buefy', 'Vue', 'Nuxt'],
-                link: 'http://testlink.io',
-                name: 'Project 2',
-                progress: 7,
-                summary: 'A project to hold other projects.',
-              },
-              {
-                id: '3',
-                description: 'A project to hold other projects.',
-                github: '',
-                images: [
-                  'projects/gzmpo4abib15yesjaeep',
-                  'projects/sh2dnhcx9nqzypl7ysqw',
-                ],
-                languages: ['HTML', 'CSS', 'Bulma', 'Buefy', 'Vue', 'Nuxt'],
-                link: 'http://testlink.io',
-                name: 'Project 3',
-                progress: 7,
-                summary: 'A project to hold other projects.',
-              },
-            ])
-            resolve()
-          }, 1000)
-        })
-      },
-      setProjects(vuexContext, projects) {
-        vuexContext.commit('setProjects', projects)
-      },
-    },
-    getters: {
-      loadedProjects(state) {
-        return state.loadedProjects
-      },
-    },
-  })
+export const mutations = {
+  setProjects(state, projects) {
+    state.loadedProjects = projects
+  },
 }
 
-export default createStore
+export const actions = {
+  nuxtServerInit(vuexContext, context) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        vuexContext.commit('setProjects', [
+          {
+            id: '1',
+            description: 'A project to hold other projects.',
+            github: '',
+            images: [
+              'projects/gzmpo4abib15yesjaeep',
+              'projects/sh2dnhcx9nqzypl7ysqw',
+            ],
+            languages: ['HTML', 'CSS', 'Bulma', 'Buefy', 'Vue', 'Nuxt'],
+            link: 'http://testlink.io',
+            name: 'The Project of Projects',
+            progress: 7,
+            summary: 'A project to hold other projects.',
+          },
+          {
+            id: '2',
+            description: 'A project to hold other projects.',
+            github: '',
+            images: [
+              'projects/gzmpo4abib15yesjaeep',
+              'projects/sh2dnhcx9nqzypl7ysqw',
+            ],
+            languages: ['HTML', 'CSS', 'Bulma', 'Buefy', 'Vue', 'Nuxt'],
+            link: 'http://testlink.io',
+            name: 'Project 2',
+            progress: 7,
+            summary: 'A project to hold other projects.',
+          },
+          {
+            id: '3',
+            description: 'A project to hold other projects.',
+            github: '',
+            images: [
+              'projects/gzmpo4abib15yesjaeep',
+              'projects/sh2dnhcx9nqzypl7ysqw',
+            ],
+            languages: ['HTML', 'CSS', 'Bulma', 'Buefy', 'Vue', 'Nuxt'],
+            link: 'http://testlink.io',
+            name: 'Project 3',
+            progress: 7,
+            summary: 'A project to hold other projects.',
+          },
+        ])
+        resolve()
+      }, 1000)
+    })
+  },
+  setProjects(vuexContext, projects) {
+    vuexContext.commit('setProjects', projects)
+  },
+}
+
+export const getters = {
+  loadedProjects(state) {
+    return state.loadedProjects
+  },
+}
