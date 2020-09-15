649245802a0396eeba14c5f72ae6e42ba281a147
Evan Ang <alethiologist@gmail.com>
2020-09-15 19:47:44 +0800

    added in a functioning delete system, cleaned up layout on admin page

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -27,12 +27,13 @@
         >
           <b-table-column
             v-slot="props"
-            field="id"
-            label="ID"
+            centered
+            field="index"
+            label="No."
             width="40"
             numeric
           >
-            {{ props.row.id }}
+            {{ props.index + 1 }}
           </b-table-column>
 
           <b-table-column v-slot="props" field="name" label="Project Name">
@@ -43,21 +44,28 @@
             {{ props.row.summary }}
           </b-table-column>
 
-          <b-table-column v-slot="props" field="status" label="Status">
+          <b-table-column v-slot="props" field="status" label="Status" centered>
             {{ props.row.progress * 10 }}%
           </b-table-column>
 
-          <b-table-column v-slot="props" custom-key="actions">
-            <button class="button is-small is-light">
-              <nuxt-link :to="'/admin/' + props.row.id">
-                <b-icon icon="edit" size="is-small" type="is-primary"></b-icon
+          <b-table-column
+            v-slot="props"
+            label="Actions"
+            centered
+            custom-key="actions"
+          >
+            <button class="button is-light">
+              <nuxt-link :to="'/projects/' + props.row.id">
+                <b-icon icon="eye" type="is-success"></b-icon
               ></nuxt-link>
             </button>
-            <button
-              class="button is-small is-light"
-              @click="deleteProject(props.row)"
-            >
-              <b-icon icon="trash" size="is-small" type="is-danger"></b-icon>
+            <button class="button is-light">
+              <nuxt-link :to="'/admin/' + props.row.id">
+                <b-icon icon="edit" type="is-primary"></b-icon
+              ></nuxt-link>
+            </button>
+            <button class="button is-light" @click="deleteProject(props.row)">
+              <b-icon icon="trash" type="is-danger"></b-icon>
             </button>
           </b-table-column>
         </b-table>
