72fe8a87f830e8df978d853b4dc58959ce06dcf8
Evan Ang <alethiologist@gmail.com>
2020-09-18 14:01:14 +0800

    fixed up homepage

--- pages/index.vue
+++ pages/index.vue
@@ -3,26 +3,31 @@
     <!-- Hero -->
     <HeroSection v-bind="hero" />
     <!-- Read more about me -->
-    <section class="section is-medium has-background-dark">
+    <section class="section has-background-dark">
       <div class="container">
-        <div class="level">
-          <div class="level-left">
-            <h1 class="title has-text-light">Wanna know more about me?</h1>
+        <div class="columns is-vcentered">
+          <div class="column is-4">
+            <img src="~/assets/images/cat-work.png" />
           </div>
-          <div class="level-right">
-            <div class="columns">
-              <div class="column">
-                <b-button type="is-success is-large" outlined>Yeah!</b-button>
-              </div>
-              <div class="column">
-                <b-button type="is-danger is-large" outlined>Nah!</b-button>
-              </div>
-            </div>
+          <div class="column">
+            <h5 class="subtitle is-5 has-text-light">
+              I'm a hobbiyist full-stack developer based in Singapore looking to
+              get a bit more professional about things.
+            </h5>
+            <h5 class="subtitle is-5 has-text-light is-spaced">
+              A lot of my career so far has consisted of suggesting web and
+              digital ideas to people. So I figured, why not learn how to just
+              make them myself?
+            </h5>
+            <h2 class="title is-3 has-text-light">
+              Wanna know more about me?
+              <nuxt-link to="/about">Click here!</nuxt-link>
+            </h2>
           </div>
         </div>
       </div>
     </section>
-    <!-- All Projects -->
+    <!-- Latest Projects -->
     <section class="section">
       <div class="container">
         <p class="title">Latest Projects</p>
@@ -47,8 +52,8 @@ export default {
           'An eternal work in progress of things I want to work on, things I am currently working on, things I have forgotten to work on, and things I have finished working on.',
         catPic: require(`~/assets/images/cat-idea.png`),
         hasButton: true,
-        buttonText: 'All projects',
-        buttonLink: 'projects',
+        buttonText: 'Find out more',
+        buttonLink: 'projects/the-project-of-projects',
       },
     }
   },
